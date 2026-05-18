7ce7dbe38c75cb5c973ce1c51e140ee352642f39
Polwatta Abhayarathna <10953578@students.plymouth.ac.uk>
2026-05-18 07:11:40 +0530

    fix: move use client before dynamic export

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
-export const dynamic = 'force-dynamic'
-
 "use client";
+
+export const dynamic = 'force-dynamic'
 
 import React, { useState, useEffect, Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
